e3205e3c6957574c33bf4cfd4911acde3f0949d2
boudokhaneeya02-hub <boudokhaneeya02@gmail.com>
2026-07-01 21:41:01 +0100

    Update urls.py

--- anticoag_suivi/urls.py
+++ anticoag_suivi/urls.py
@@ -1,24 +1,21 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import RedirectView
-from django.views.generic import TemplateView
-
-from . import views
+from django.views.generic import RedirectView, TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("sw.js", views.service_worker, name="service_worker"),
     path("comptes/", include("accounts.urls")),
     path("patients/", include("patients.urls")),
     path("suivi/", include("consultations.urls")),
     path("notifications/", include("notifications.urls")),
     path("", RedirectView.as_view(pattern_name="patients:liste", permanent=False)),
+
     path(
-    "service-worker.js",
-    TemplateView.as_view(
-        template_name="service-worker.js",
-        content_type="application/javascript"
+        "service-worker.js",
+        TemplateView.as_view(
+            template_name="service-worker.js",
+            content_type="application/javascript"
+        ),
+        name="service_worker"
     ),
-    name="service_worker"
-),
 ]
